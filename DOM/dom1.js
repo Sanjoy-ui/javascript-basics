4db94185856eeeaa813e38a6cf147037ad74e084
Sanjoy Deb <sanjoydeb404@gmail.com>
2025-07-06 11:39:28 +0530

    Add files via upload
    DOM

---
+++ DOM/dom1.js
@@ -0,0 +1,25 @@
+document.getElementById().innerHTML = "sanjoy"
+document.getElementById().id
+document.getElementById().className
+document.getElementById().getAttribute('ATTRIBUTE Name')
+document.getElementById().setAttribute('ATTRIBUTE Name , new name')
+
+const title = document.getElementById("name");
+title.textContent;       // content bair korte use krommo  // hidden content takle eda o get koira dibo
+
+title.innerText;    // jeda dekha jaitase shudo oi content 
+
+document.getElementsByClassName("")
+document.querySelector("")
+document.querySelectorAll("")
+
+const list_item = document.querySelector("ul");          // list item r modhe manipulation
+list_item.forEach((li)=>{
+    li.style.backgroundColor = "red"
+})
+
+list_item[0].backgroundColor= "green";       // individual item selection
+
+const ulLi = Array.from(tempClassArray); // htmlcollection to array conversion. 
+
+
